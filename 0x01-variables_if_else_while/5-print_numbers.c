a0e72c09a8705aaa80c012fc3aff157656a16e7d
SirJupiter <jupitertoby@gmail.com>
2023-10-06 11:34:04 +0100

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *  main - This is the main function for entry
+ *
+ *  Return: 0 on successful execution of code
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}
