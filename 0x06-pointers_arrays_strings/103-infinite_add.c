d3e15b91bd3dade1e41552961dd89fe9eeabc726
SirJupiter <jupitertoby@gmail.com>
2023-10-18 22:25:29 +0100

    It is the addition of strangeness to beauty that constitutes the romantic character in art

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,53 @@
+#include "main.h"
+
+/**
+ * infinite_add - function that adds two numbers
+ * @n1: first number
+ * @n2: second number
+ * @r: buffer for storing result
+ * @size_r: size of buffer
+ *
+ * Return: addition of numbers
+*/
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int i = 0, j = 0, carry = 0, digits = 0;
+	int var1 = 0, var2 = 0, temp = 0;
+
+	while (*(n1 + i) != '\0')
+		i++;
+	while (*(n2 + j) != '\0')
+		j++;
+	i--;
+	j--;
+	if (j >= size_r || i >= size_r)
+		return (0);
+
+	while (j >= 0 || i >= 0 || carry == 1)
+	{
+		if (i < 0)
+			var1 = 0;
+		else
+			var1 = *(n1 + i) - '0';
+		if (j < 0)
+			var2 = 0;
+		else
+			var2 = *(n2 + j) - '0';
+		temp = var1 + var2 + carry;
+		if (temp >= 10)
+			carry = 1;
+		else
+			carry = 0;
+		if (digits >= (size_r - 1))
+			return (0);
+		*(r + digits) = (temp % 10) + '0';
+		digits++;
+		j--;
+		i--;
+	}
+	if (digits == size_r)
+		return (0);
+	*(r + digits) = '\0';
+	rev_string(r);
+	return (r);
+}
