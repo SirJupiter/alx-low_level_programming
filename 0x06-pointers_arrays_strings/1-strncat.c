7abfd70d96cadedb774867518cc6141cc7469871
SirJupiter <jupitertoby@gmail.com>
2023-10-18 19:04:00 +0100

    Mandatory tasks

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,31 +10,18 @@
 */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i, j, k;
+	int i, j;
 
 	i = 0;
 	while (dest[i] != '\0')
 		i++;
-	j = 0;
-	while (src[j] != '\0')
-		j++;
 
-	if (j < n)
+	for (j = 0; j < n && src[j] != '\0'; j++)
 	{
-		for (k = 0; src[k] != '\0'; k++)
-		{
-			dest[i] = src[k];
-			i++;
-		}
-		dest[i] = '\0';
-	} else
-	{
-		for (k = 0; k <= n; k++)
-		{
-			dest[i] = src[k];
-			i++;
-		}
+		dest[i] = src[j];
+		i++;
 	}
+	dest[i] = '\0';
 
 	return (dest);
 }
