a61521ac69382a2c46346c22231d4f6364c7912d
SirJupiter <jupitertoby@gmail.com>
2023-10-18 19:23:45 +0100

    Mandatory tasks

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -22,6 +22,8 @@ char *cap_string(char *str)
 				if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
 				{
 					str[i + 1] = str[(i + 1)] - 32;
+					if (str[i] == '\t')
+						str[i] = ' ';
 				}
 			}
 		}
