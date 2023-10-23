96052787ae10d173ceaa35627ca943a2892c591c
SirJupiter <jupitertoby@gmail.com>
2023-10-23 12:56:05 +0100

    6. Chess is mental torture

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -6,5 +6,19 @@
 */
 void print_chessboard(char (*a)[8])
 {
+	int i, j, c, d;
 
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			c = a[i][j] >= 'a' && a[i][j] <= 'z';
+			d = a[i][j] >= 'A' && a[i][j] <= 'Z';
+			if (c || d)
+				_putchar(a[i][j]);
+			else
+				_putchar(' ');
+		}
+		_putchar('\n');
+	}
 }
