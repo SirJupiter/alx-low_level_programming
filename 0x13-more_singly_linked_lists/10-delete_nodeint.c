b3beacf7f88088e3c765c8bc69e137907f3a7a78
SirJupiter <jupitertoby@gmail.com>
2023-11-20 19:07:09 +0100

    10. Delete at index

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -13,6 +13,8 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	unsigned int i;
 	listint_t *current, *prev;
 
+	if (head == NULL)
+		return (-1);
 	if (*head == NULL)
 		return (-1);
 
@@ -40,5 +42,5 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		current = NULL;
 		return (1);
 	}
-	return (-1);
+	return (1);
 }
