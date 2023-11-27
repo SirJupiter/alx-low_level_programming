9392d8b35f982cab562b4a3d78177335e3c60f0b
SirJupiter <jupitertoby@gmail.com>
2023-11-27 17:58:00 +0100

    1. Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,30 +9,24 @@
 */
 int create_file(const char *filename, char *text_content)
 {
-	int file_desc;
+	int file_desc, length = 0;
 	ssize_t write_text;
 
 	if (filename == NULL)
-	{
 		return (-1);
-	}
-	else
-	{
-		if ((open(filename, O_CREAT | O_EXCL)) == -1)
-		{
-			file_desc = open(filename, O_TRUNC);
-		}
-		else
-		{
-			file_desc = open(filename, O_CREAT | O_RDWR, 0600);
-		}
-	}
+
+	file_desc = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+
 	if (file_desc == -1)
 		return (-1);
 
 	if (text_content != NULL)
 	{
-		write_text = write(file_desc, text_content, strlen(text_content));
+		while (text_content[length])
+			length++;
+
+		write_text = write(file_desc, text_content, length);
+
 		if (write_text == -1)
 		{
 			close(file_desc);
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -4,10 +4,6 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int _putchar(char c);
