cd6fbb6b932324019888b80f35df817983de6c11
SirJupiter <jupitertoby@gmail.com>
2023-11-27 18:13:53 +0100

    1. Under the snow

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,39 +7,35 @@
  * @filename: pointer to the filename
  * @letters: number of letters it should print
  *
- * Return: number of letters printed
+ * Return: number of letters written_text
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int file_desc;
-	ssize_t read_text, written_text;
-	char *buffer;
+	FILE *the_file;
+	ssize_t written_text = 0, i = 0;
+	char chr;
 
-	if (filename == NULL)
-		return (0);
-
-	file_desc = open("filename", O_RDONLY);
-	if (file_desc == -1)
-		return (0);
-
-	buffer = malloc(letters * sizeof(char));
-	if (buffer == NULL)
+	if (!filename || letters < 1)
 	{
 		return (0);
 	}
 
-	read_text = read(file_desc, buffer, letters);
-	if (read_text == -1)
-	{
+	the_file = fopen(filename, "r");
+
+	if (!the_file)
 		return (0);
+
+	while ((chr = fgetc(the_file)) != EOF)
+	{
+		if (written_text < (ssize_t)letters)
+		{
+			i = write(STDOUT_FILENO, &chr, 1);
+			if (i == -1)
+				return (0);
+			written_text += i;
+		}
 	}
 
-	written_text = write(STDOUT_FILENO, buffer, read_text);
-	if (written_text == -1)
-		return (0);
-
-	free(buffer);
-	close(file_desc);
-
+	fclose(the_file);
 	return (written_text);
 }
