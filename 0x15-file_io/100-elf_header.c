6a749142aa1b0e4c5fc4f7a8937333229c8299b6
SirJupiter <jupitertoby@gmail.com>
2023-11-28 01:22:01 +0100

    4. Elf

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,65 +1,80 @@
-#include "main.h"
-#include <elf.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <elf.h>
 #include <string.h>
 
-#define ERROR_STATUS 98
-#define ELF_MAGIC "\177ELF"
+/**
+ * display_error - displays error message
+ * @msg: error message
+*/
+void display_error(const char *msg)
+{
+	fprintf(stderr, "%s\n", msg);
+	exit(98);
+}
 
 /**
- * main - main function for entry
- * @argc: number of arguments
- * @argv: array of pointers to arguments
- *
- * Return: 0 on successful execution
- */
-
-int main(int argc, char *argv[])
+ * display_elf_header - displays elf header
+ * @filename: name of file
+*/
+void display_elf_header(const char *filename)
 {
-	int file_desc, iterator, no_of_bytes_read;
-	Elf64_Ehdr _the_header;
+	int file_desc, iterator;
+	Elf64_Ehdr elf_header;
 
-	if (argc != 2)
-	{
-		fprintf(stderr, "Usage:elf_header elf_filename\n");
-		return (ERROR_STATUS);
-	}
-
-	file_desc = open(argv[1], O_RDONLY);
+	file_desc = open(filename, O_RDONLY);
 	if (file_desc == -1)
-	{
-		fprintf(stderr, "Error: Can't open file\n");
-		return (ERROR_STATUS);
-	}
-	no_of_bytes_read = read(file_desc, &_the_header, sizeof(_the_header));
-	if (no_of_bytes_read == -1)
-	{
-		fprintf(stderr, "Error: Can't read file\n");
-		close(file_desc);
-		return (ERROR_STATUS);
-	}
+		display_error("Error opening file");
 
-	if (memcmp(_the_header.e_ident, ELF_MAGIC, strlen(ELF_MAGIC)) != 0)
-	{
-		fprintf(stderr, "Error: Not an ELF file\n");
-		return (ERROR_STATUS);
-	}
-
-	printf("Magic: ");
-	for (iterator = 0; iterator < EI_NIDENT; iterator++)
-	{
-		printf("%02x ", _the_header.e_ident[iterator]);
-	}
-	putchar('\n');
-
-	printf("Class:                             %s\n", _the_header.e_ident[EI_CLASS] == ELFCLASS32 ? "ELF32" : "ELF64");
-	printf("Data:                              %s\n", _the_header.e_ident[EI_DATA] == ELFDATA2LSB ? "2's complement, little endian" : "2's complement, big endian");
-	printf("Version:                           %d\n", _the_header.e_ident[EI_VERSION]);
-	printf("OS/ABI:                            UNIX - System V\n");
-	printf("ABI Version:                       %d\n", _the_header.e_ident[EI_OSABI]);
-	printf("Type:                              %s\n", _the_header.e_type == ET_REL ? "REL (Relocatable file)" : _the_header.e_type == ET_EXEC ? "EXEC (Executable file)" : _the_header.e_type == ET_DYN ? "DYN (Shared object file)" : "None");
-	printf("Entry point address:               %#lx\n", _the_header.e_entry);
+	if (read(file_desc, &elf_header, sizeof(Elf64_Ehdr)) != sizeof(Elf64_Ehdr))
+		display_error("Error reading ELF header");
 
 	close(file_desc);
+
+	/*Check ELF magic number*/
+	if (memcmp(elf_header.e_ident, ELFMAG, SELFMAG) != 0)
+		display_error("Not an ELF file");
+
+	/* Display ELF header information*/
+	printf("ELF Header:\n");
+	printf("  Magic:   ");
+	for (iterator = 0; iterator < EI_NIDENT; ++iterator)
+		printf("%02x ", elf_header.e_ident[iterator]);
+
+	printf("\n");
+	printf("  Class:                             %s\n",
+	elf_header.e_ident[EI_CLASS] == ELFCLASS32 ? "ELF32" : "ELF64");
+	printf("  Data:                              %s\n",
+	elf_header.e_ident[EI_DATA] == ELFDATA2LSB ? "2's complement, little endian"
+	: "2's complement, big endian");
+	printf("  Version:                           %d (current)\n",
+	elf_header.e_ident[EI_VERSION]);
+	printf("  OS/ABI:                            %s\n",
+	elf_header.e_ident[EI_OSABI] == ELFOSABI_SYSV ? "UNIX - System V" : "Other");
+	printf("  ABI Version:                       %d\n",
+	elf_header.e_ident[EI_ABIVERSION]);
+	printf("  Type:                              %s\n",
+	elf_header.e_type == ET_EXEC ? "EXEC (Executable file)" : "Other");
+	printf("  Entry point address:               %#lx\n",
+	(unsigned long)elf_header.e_entry);
+
+}
+
+/**
+ * main - main function
+ * @argc: arggc
+ * @argv: argv
+ *
+ * Return: 0 on successful execution
+*/
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+		display_error("Usage: elf_header elf_filename");
+
+	display_elf_header(argv[1]);
+
 	return (0);
 }
