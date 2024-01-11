f1487d16fce0d36c6564e9570ae677d14d2980f9
SirJupiter <jupitertoby@gmail.com>
2024-01-11 17:53:15 +0100

    7. Insert at index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -15,7 +15,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (!h)
 		return (NULL);
-
+	if (idx == 0)
+		return (add_dnodeint(h, n));
 	insert = malloc(sizeof(dlistint_t));
 	if (!insert)
 		return (NULL);
