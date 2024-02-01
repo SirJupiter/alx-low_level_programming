6e998931010267ac048f39905be9a214efe20436
SirJupiter <jupitertoby@gmail.com>
2024-02-01 18:11:10 +0100

    0. >>> ht = {}

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,7 +11,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	unsigned long int iterator;
 	hash_table_t *ht = malloc(sizeof(hash_table_t));
 
-	if (ht == NULL || size <= 0)
+	if (ht == NULL)
 		return (NULL);
 
 	ht->size = size;
