8cb756f59c3331eeb5b58bacfb2cf4dadec62ddd
SirJupiter <jupitertoby@gmail.com>
2024-02-01 17:57:02 +0100

    0. >>> ht = {}

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - function creates a hash table
+ * @size: size of the hash table
+ *
+ * Return: pointer to struct hash_table_s or NULL on error
+*/
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	unsigned int iterator;
+	hash_table_t *ht = malloc(sizeof(hash_table_t));
+
+	if (ht == NULL)
+		return (NULL);
+
+	if (size <= 0)
+		return (NULL);
+
+	ht->size = size;
+
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+	if (ht->array == NULL)
+		return (NULL);
+
+	for  (iterator = 0; iterator < ht->size; iterator++)
+		ht->array[iterator] = NULL;
+
+	return (ht);
+}
