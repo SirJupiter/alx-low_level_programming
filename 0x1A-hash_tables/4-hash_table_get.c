9e9bebe6644a86f15171569537578a921591319b
SirJupiter <jupitertoby@gmail.com>
2024-02-02 15:50:07 +0100

    4. >>> ht['betty']

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - function retrieves a value associated with a key
+ * @ht: pointer to the hashtable into which we need to look
+ * @key: key for which we are looking
+ *
+ * Return: the corresponding value if found, NULL otherwise
+*/
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int iterator;
+	hash_node_t *temp;
+
+	iterator  = key_index((unsigned char *)key, ht->size);
+
+	if (ht->array[iterator] != NULL)
+	{
+		temp = ht->array[iterator];
+		while (temp != NULL)
+		{
+			if (strcmp(temp->key, key) == 0)
+				return (temp->value);
+			temp = temp->next;
+
+		}
+		return (NULL);
+	}
+
+	return (NULL);
+}
